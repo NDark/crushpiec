3b85308967dd55c466ebb1766f7f97ad02873e43
ndark <ndarkteng@gmail.com>
2016-02-12 17:15:45 +0800

    add title operation

---
+++ Client/Assets/Scripts/Title/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour 
+{
+	public string m_Scene = "" ;
+
+	public void ChangeSceneTo()
+	{
+		Application.LoadLevel(m_Scene);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
