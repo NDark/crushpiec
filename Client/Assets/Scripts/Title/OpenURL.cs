c983ce0e52ed2cd2eaaa60afe076e4b302e0e651
ndark <ndarkteng@gmail.com>
2016-02-12 16:45:21 +0800

    add title scene

---
+++ Client/Assets/Scripts/Title/OpenURL.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenURL : MonoBehaviour 
+{
+	public string m_URL = "" ;// 需指定URL字串
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Open()
+	{
+		// Debug.Log( Application.platform ) ;
+		if( Application.platform == RuntimePlatform.WindowsWebPlayer )
+		{
+			string url = "window.open('" + m_URL + "','aNewWindow')" ;
+			// Debug.Log( url ) ;
+			Application.ExternalEval( url );					
+		}
+		else /*if( Application.platform == RuntimePlatform.WindowsPlayer ||
+				 Application.platform == RuntimePlatform.WindowsEditor )*/
+		{
+			Application.OpenURL( m_URL ) ;		
+		}
+	}
+}
---
+++ Client/Assets/Scripts/Title/ReadVersion.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReadVersion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		TextAsset ta = Resources.Load("version") as TextAsset;
+		if( null != ta )
+		{
+			UnityEngine.UI.Text text = this.GetComponent<UnityEngine.UI.Text>() ;
+			if( null != text )
+			{
+				text.text = ta.text ;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
