faf351b028956c519788ad06019652331e9388ee
KasakiTzeng <valkidy@gmail.com>
2016-02-13 17:19:32 +0800

    update part body animation

--- Client/Assets/Scripts/Battle/BoneAnimation.cs
+++ Client/Assets/Scripts/Battle/BoneAnimation.cs
@@ -11,16 +11,28 @@ public class BoneAnimation : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+
+        foreach (Transform child in transform) {
+            Debug.LogError(child.position + "  ,  " + transform.position);
+            Vector3 forceDir = (child.position - transform.position) * Random.Range(100.0f, 400.0f);
+            Rigidbody rigidBody = child.gameObject.AddComponent<Rigidbody>();
+            rigidBody.useGravity = false;
+            rigidBody.AddForce(forceDir);
+        }
+        /*
         m_Distance = Vector3.Distance(m_EndPoint, m_StartPoint);
         m_ElapsedTime = 0.0f;
+        */
     }
 	
 	// Update is called once per frame
 	void Update () {
+        /*
         m_ElapsedTime += Time.deltaTime;
         m_Rotation += (0.1f * m_ElapsedTime);
 
         transform.position = Vector3.Lerp(m_StartPoint, m_EndPoint, 
             m_Speed * m_ElapsedTime / m_Distance);
+        */
     }
 }
