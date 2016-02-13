286925596cdd47ffd4b33c7b81ee43fd297e7c3a
ndark <ndarkteng@gmail.com>
2016-02-13 16:41:22 +0800

    add display when input text

--- Client/Assets/Scripts/Language/KeyboardDetect.cs
+++ Client/Assets/Scripts/Language/KeyboardDetect.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class KeyboardDetect : MonoBehaviour {
+public class KeyboardDetect : UnityEngine.UI.InputField {
 
-	public UnityEngine.UI.Text m_Text = null ;
+	public UnityEngine.UI.Text m_DebugText = null ;
+	public UnityEngine.UI.Text m_QuestionLabel = null ;
 
 	// Use this for initialization
 	void Start () {
+		this.onEndEdit.AddListener(val =>
+		                                 {
+			if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+				Debug.Log("End edit on enter");
+		});
+
+		GameObject obj = GameObject.Find("DebugText");
+		if( null != obj )
+		{
+			m_DebugText = obj.GetComponent<UnityEngine.UI.Text>() ;
+		}
+
 
 	}
 
@@ -14,30 +27,34 @@ public class KeyboardDetect : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-
+		if( null != m_DebugText && null != m_QuestionLabel  )
+		{
+			m_DebugText.text = m_QuestionLabel.text ;
+			m_DebugText.enabled = this.isFocused ;
+		}
 	}
 
 	void OnFocus()
 	{
 		Debug.Log("OnFocus");
-		m_Text.text = "OnFocus" ;
+		if( null != m_DebugText ) m_DebugText.text = "OnFocus" ;
 	}
 
 	void OnDeselect( UnityEngine.EventSystems.BaseEventData eventData)
 	{
 		Debug.Log("OnDeselect");
-		m_Text.text = "OnDeselect" ;
+		if( null != m_DebugText ) m_DebugText.text = "OnDeselect" ;
 	}
 
 	public void OnPointerClick(UnityEngine.EventSystems.PointerEventData eventData)
 	{
 		Debug.Log("OnPointerClick");
-		m_Text.text = "OnPointerClick" ;
+		if( null != m_DebugText ) m_DebugText.text = "OnPointerClick" ;
 	}
 
 	public void OnSubmit(UnityEngine.EventSystems.BaseEventData eventData)
 	{
 		Debug.Log("OnSubmit");
-		m_Text.text = "OnSubmit" ;
+		if( null != m_DebugText ) m_DebugText.text = "OnSubmit" ;
 	}
 }
