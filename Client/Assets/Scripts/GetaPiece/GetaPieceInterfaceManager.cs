9ddfe41ddfa32857a680265ccd485614629c6f41
ndark <ndarkteng@gmail.com>
2016-10-09 13:01:25 +0800

    Client/Assets/Scripts/GetaPiece/GetaPieceInterfaceManager.cs

---
+++ Client/Assets/Scripts/GetaPiece/GetaPieceInterfaceManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GetaPieceInterfaceManager : MonoBehaviour 
+{
+	public void TrySetAction0( string _ActionStr )
+	{
+		ActionKey key = m_ActionKeyEnumHelper.GetKey( _ActionStr ) ;
+		TrySetAction( 0 , key ) ;
+	}
+
+	public void TrySetAction1( string _ActionStr )
+	{
+		ActionKey key = m_ActionKeyEnumHelper.GetKey( _ActionStr ) ;
+		TrySetAction( 1 , key ) ;
+	}
+
+	public void TrySetAction2( string _ActionStr )
+	{
+		ActionKey key = m_ActionKeyEnumHelper.GetKey( _ActionStr ) ;
+		TrySetAction( 2 , key ) ;
+	}
+	
+	public void TrySetAction( int _ComponentIndex , ActionKey _Action )
+	{
+		
+	}
+
+	
+	public void TryStart()
+	{
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private ActionKeyEnumHelper m_ActionKeyEnumHelper = new ActionKeyEnumHelper() ;
+
+}
+
+
+public enum ActionKey 
+{
+	Attack , 
+	Defend , 
+	Concentrate ,
+}
+
+public class ActionKeyEnumHelper
+{
+	public Dictionary< string , ActionKey > m_Map = null ;
+	public void Initialize()
+	{
+		m_Map = new Dictionary<string, ActionKey>() ;
+		m_Map.Add("Attack",ActionKey.Attack ) ;
+		m_Map.Add("Defend",ActionKey.Defend ) ;
+		m_Map.Add("Concentrate",ActionKey.Concentrate ) ;
+	}
+	
+	
+	public void TryInitialize()
+	{
+		if( null == m_Map )
+		{
+			Initialize() ;
+		}
+	}
+	
+	public string GetString( ActionKey _Key )
+	{
+		TryInitialize() ;
+		return _Key.ToString() ;
+	}
+	
+	public ActionKey GetKey( string _Str )
+	{
+		TryInitialize() ;
+		ActionKey ret = ActionKey.Attack ;
+		m_Map.TryGetValue( _Str , out ret ) ;
+		return ret ;
+	}
+}
