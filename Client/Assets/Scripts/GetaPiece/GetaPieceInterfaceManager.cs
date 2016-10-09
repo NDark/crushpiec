42919c459aa55313fe4375ba16f24ffd58f8dbaa
ndark <ndarkteng@gmail.com>
2016-10-09 13:57:13 +0800

    add preset button

--- Client/Assets/Scripts/GetaPiece/GetaPieceInterfaceManager.cs
+++ Client/Assets/Scripts/GetaPiece/GetaPieceInterfaceManager.cs
@@ -1,29 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+
+public enum GetaPieceInterfaceState
+{
+	UnActive = 0 ,
+	InterfaceInitialize ,
+	BattleInitialize ,
+	WaitBattleInitialize ,
+
+	EnterRound ,
+	WaitPlayerInput ,
+	EnterAnimation ,
+	WaitAnimation ,
+	JudgeVictory ,
+
+	EndGame ,
+}
+
 public class GetaPieceInterfaceManager : MonoBehaviour 
 {
+	public UnityEngine.UI.Image [] m_Component0ButtonsImages = null ;
+	public UnityEngine.UI.Image [] m_Component1ButtonsImages = null ;
+	public UnityEngine.UI.Image [] m_Component2ButtonsImages = null ;
+
+	private ActionKey [] m_SelectedActions = new ActionKey[3] ;
+
 	public void TrySetAction0( string _ActionStr )
 	{
 		ActionKey key = m_ActionKeyEnumHelper.GetKey( _ActionStr ) ;
-		TrySetAction( 0 , key ) ;
+
+		PressComponentButton( 0 , key ) ;
+
 	}
 
 	public void TrySetAction1( string _ActionStr )
 	{
 		ActionKey key = m_ActionKeyEnumHelper.GetKey( _ActionStr ) ;
-		TrySetAction( 1 , key ) ;
+
+		PressComponentButton( 1 , key ) ;
+
 	}
 
 	public void TrySetAction2( string _ActionStr )
 	{
 		ActionKey key = m_ActionKeyEnumHelper.GetKey( _ActionStr ) ;
-		TrySetAction( 2 , key ) ;
+
+		PressComponentButton( 2 , key ) ;
+
 	}
 	
 	public void TrySetAction( int _ComponentIndex , ActionKey _Action )
 	{
 		Debug.Log("TrySetAction _ComponentIndex=" + _ComponentIndex + " _Action=" + _Action );
+		m_SelectedActions[ _ComponentIndex ] = _Action ;
 
 	}
 
@@ -32,26 +62,152 @@ public class GetaPieceInterfaceManager : MonoBehaviour
 	{
 		Debug.Log("TryStart" );
 
+		m_State = GetaPieceInterfaceState.EnterAnimation ;
 	}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		/*
+		public enum GetaPieceInterfaceState
+		{
+			UnActive = 0 ,
+			InterfaceInitialize ,
+			BattleInitialize ,
+			WaitBattleInitialize ,
+			
+			EnterRound ,
+			WaitPlayerInput ,
+			EnterAnimation ,
+			WaitAnimation ,
+			JudgeVictory ,
+			
+			EndGame ,
+		}
+		//*/
+
+		switch( m_State ) 
+		{
+		case GetaPieceInterfaceState.UnActive :
+			m_State = GetaPieceInterfaceState.InterfaceInitialize ;
+			break ;
+		case GetaPieceInterfaceState.InterfaceInitialize :
+			InterfaceInitialize() ;
+			break ;
+		case GetaPieceInterfaceState.BattleInitialize :
+			BattleInitialize() ;
+			break ;
+		case GetaPieceInterfaceState.WaitBattleInitialize :
+			WaitBattleInitialize() ;
+			break ;
+		case GetaPieceInterfaceState.EnterRound :
+			EnterRound() ;
+			break ;
+		case GetaPieceInterfaceState.WaitPlayerInput :
+			break ;
+		case GetaPieceInterfaceState.EnterAnimation :
+			EnterAnimation() ;
+			break ;
+		case GetaPieceInterfaceState.WaitAnimation :
+			WaitAnimation() ;
+			break ;
+		case GetaPieceInterfaceState.JudgeVictory :
+			JudgeVictory() ;
+			break ;
+		case GetaPieceInterfaceState.EndGame : 
+			break ;
+
+
+		}
+
+	}
+
+	private void InterfaceInitialize()
+	{
+		m_State = GetaPieceInterfaceState.BattleInitialize ;
+	}
 	
+	private void BattleInitialize()
+	{
+		m_State = GetaPieceInterfaceState.WaitBattleInitialize ;
+	}
+	
+	private void WaitBattleInitialize()
+	{
+		m_State = GetaPieceInterfaceState.EnterRound ;
+	}
+	
+	private void EnterRound()
+	{
+		
+		PressComponentButton( 0 , ActionKey.Concentrate ) ;
+		PressComponentButton( 1 , ActionKey.Concentrate ) ;
+		PressComponentButton( 2 , ActionKey.Concentrate ) ;
+
+		m_State = GetaPieceInterfaceState.WaitPlayerInput ;
+	}
+	
+	private void EnterAnimation()
+	{
+		m_State = GetaPieceInterfaceState.WaitAnimation ;
+	}
+	
+	private void WaitAnimation()
+	{
+		m_State = GetaPieceInterfaceState.JudgeVictory ;
+	}
+	
+	private void JudgeVictory()
+	{
+		m_State = GetaPieceInterfaceState.EnterRound ;
+		// m_State = GetaPieceInterfaceState.EndGame ;
+
+	}
+
+	
+	private void PressComponentButton( int _ComponentIndex , ActionKey _Action )
+	{
+		int keyIndex = (int) _Action ;
+		UnityEngine.UI.Image [] _Images = null ;
+		switch( _ComponentIndex )
+		{
+		case 0 :
+			_Images = m_Component0ButtonsImages ;
+			break ;
+		case 1 :
+			_Images = m_Component1ButtonsImages ;
+			break ;
+		case 2 :
+			_Images = m_Component2ButtonsImages ;
+			break ;
+		}
+		SetColorForSelectComponentButtons( _Images , keyIndex ) ;
+		
+		TrySetAction( _ComponentIndex , _Action ) ;
+	}
+
+	private void SetColorForSelectComponentButtons( UnityEngine.UI.Image [] _ImageArray , int _SelectIndex )
+	{
+		if( null == _ImageArray )
+		{
+			return ;
+		}
+
+		for( int i = 0 ; i < _ImageArray.Length ; ++i )
+		{
+			_ImageArray[ i ].color = ( i != _SelectIndex ) ? ( Color.white ) : (Color.green) ;
+		}
 	}
 
 	private ActionKeyEnumHelper m_ActionKeyEnumHelper = new ActionKeyEnumHelper() ;
-
+	private GetaPieceInterfaceState m_State = GetaPieceInterfaceState.UnActive ;
 }
 
 
 public enum ActionKey 
 {
-	Attack , 
+	Attack = 0 , 
 	Defend , 
 	Concentrate ,
 }
