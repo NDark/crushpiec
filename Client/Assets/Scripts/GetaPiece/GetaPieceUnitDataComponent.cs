987266d0409703d8997b0a4ff8f665890d37a49b
ndark <ndarkteng@gmail.com>
2016-10-10 15:40:42 +0800

    move GetaPieceConst to

--- Client/Assets/Scripts/GetaPiece/GetaPieceUnitDataComponent.cs
+++ Client/Assets/Scripts/GetaPiece/GetaPieceUnitDataComponent.cs
@@ -163,24 +163,3 @@ public class GetaPieceUnitData
 	private int m_HitPoint = 0 ;
 	private int m_Energy = 0 ;
 }
-
-public static class GetaPieceConst
-{
-	public static int INIT_ENERGY = 10 ;
-	public static int INIT_HITPOINT = 10 ;
-
-	public static int COST_ENERGY_ATTACK = 3 ;
-	public static int COST_ENERGY_DEFEND = 1 ;
-	public static int COST_ENERGY_CONCENTRATE = 0 ;
-	
-	public static int BUFF_ENERGY_ATTACK_TO_DEFEND = 1 ;
-	public static int ENERGY_REFILL_EACH_TURN = 3 ;
-	
-	public static int DAMAGE_ATTACK_TO_ATTACK = 2 ;
-	public static int DAMAGE_ATTACK_TO_CONCENTRATE = 2 ;
-	public static int DAMAGE_ATTACK_TO_DEFEND = 0 ;
-	public static int DAMAGE_POWER_ATTACK_TO_CONCENTRATE = 3 ;
-	public static int DAMAGE_POWER_ATTACK_TO_ATTACK = 3 ;
-	public static int DAMAGE_POWER_ATTACK_TO_DEFEND = 1 ;
-	
-}
