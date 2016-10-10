d9a865be0ead7dcda479b7d4bcd71efedebd0ca9
ndark <ndarkteng@gmail.com>
2016-10-10 10:40:35 +0800

    add virtual class methods

--- Client/Assets/Scripts/Language/DummyBattlePlay.cs
+++ Client/Assets/Scripts/Language/DummyBattlePlay.cs
@@ -3,28 +3,31 @@ using System.Collections;
 
 public class DummyBattlePlay : MonoBehaviour 
 {
+	public virtual bool IsInitialized() 
+	{
+		return false ;
+	}
+
 	public virtual bool IsInAnimation() 
 	{
 		return false ;
 	}
 
+	public virtual void StartBattle() 
+	{
+
+	}
+	 
+	// deprecated
 	public virtual void Attack( float _Ratio ) 
 	{
 		Debug.Log("Attack() _Ratio" + _Ratio );
 	}
 
+	// deprecated
 	public virtual void Defend( float _Ratio ) 
 	{
 		Debug.Log("Defend() _Ratio" + _Ratio );
 	}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
