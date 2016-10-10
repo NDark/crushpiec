4894add4523a0c65bd9daf91ca7cef1853af3f64
ndark <ndarkteng@gmail.com>
2016-10-10 12:49:04 +0800

    add GetaPieceBattleEvent

---
+++ Client/Assets/Scripts/GetaPiece/GetaPieceBattleEvent.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GetaPieceBattleEvent
+{
+	public string Type {get;set;} 
+	public string Target {get;set;}
+	public string Value {get;set;}
+
+	public GetaPieceBattleEventType GetType()
+	{
+		return GetaPieceBattleEventTypeEnumHelperHub.m_Helper.GetKey( this.Type ) ;
+	}
+
+	public int AsInt()
+	{
+		int ret = 0 ;
+		int.TryParse( this.Value , out ret ) ;
+		return ret ;
+	}
+	
+	public float AsFloat()
+	{
+		float ret = 0 ;
+		float.TryParse( this.Value , out ret ) ;
+		return ret ;
+	}
+}
+
+
+public enum GetaPieceBattleEventType
+{
+	Damage,
+}
+
+
+public class GetaPieceBattleEventTypeEnumHelper
+{
+	public Dictionary< string , GetaPieceBattleEventType > m_Map = null ;
+	public void Initialize()
+	{
+		m_Map = new Dictionary<string, GetaPieceBattleEventType>() ;
+		m_Map.Add("Damage",GetaPieceBattleEventType.Damage ) ;
+		
+	}
+	
+	
+	public void TryInitialize()
+	{
+		if( null == m_Map )
+		{
+			Initialize() ;
+		}
+	}
+	
+	public string GetString( ActionKey _Key )
+	{
+		TryInitialize() ;
+		return _Key.ToString() ;
+	}
+	
+	public GetaPieceBattleEventType GetKey( string _Str )
+	{
+		TryInitialize() ;
+		GetaPieceBattleEventType ret = GetaPieceBattleEventType.Damage ;
+		m_Map.TryGetValue( _Str , out ret ) ;
+		return ret ;
+	}
+}
+
+public static class GetaPieceBattleEventTypeEnumHelperHub
+{
+	public static GetaPieceBattleEventTypeEnumHelper m_Helper = new GetaPieceBattleEventTypeEnumHelper() ;
+}
